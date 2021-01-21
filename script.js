a4229057918d668536ecbba33837344a41c729a2
TanYa-Go <tanja.godinic@gmail.com>
2021-01-21 22:56:39 +0000

    Create maths game

---
+++ script.js
@@ -0,0 +1,132 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName('button');
+    let gameType = "addition";
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if(this.getAttribute('data-type') === 'submit') {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute('data-type');
+                runGame(gameType);         
+            }
+        })
+    }
+    document.getElementById('answer-box').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            checkAnswer();
+        }
+    })
+    runGame('addition');
+})
+
+function runGame(gameType) {
+
+    document.getElementById('answer-box').value = '';
+    document.getElementById('answer-box').focus();
+
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1,num2);
+    } else if (gameType === 'multiply') {
+        displayMultiplynQuestion(num1,num2);
+    } else if (gameType === 'subtract') {
+        displaySubtractQuestion(num1,num2);
+    } else if (gameType === 'division') {
+        displayDivisionQuestion(num1,num2);
+    } else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting!`
+    }
+}
+
+
+function checkAnswer(){
+    // Checks the answer against the first element in 
+    // the returned claculateCorrectAnswer array
+
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    
+    if (isCorrect) {
+        alert ('Hey, you got it right! :D');
+        incrementScore();
+    }  else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
+    }
+    runGame(calculatedAnswer[1]);
+}
+
+function calculateCorrectAnswer() {
+
+    // Gets the operands (the numbers) and the operator (plus,minus etc)
+    // directly from the DOM
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === '+') {
+         return [operand1 + operand2, 'addition'];
+    } else if (operator === 'x') {
+        return [operand1 * operand2, 'multiply'];
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
+    } else if (operator === '/') {
+        return [parseInt(operand1 / operand2),'division'];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}, aborting!`;
+    }
+
+}
+
+function incrementScore() {
+    // Gets the current score from the DOM and increments it
+    
+     let oldScore = parseInt(document.getElementById('score').innerText);
+     document.getElementById('score').innerText = ++oldScore;
+
+}
+
+function incrementWrongAnswer() {
+    // Gets the current tally of incorrect anwers from the DOM and increments it
+    
+     let oldScore = parseInt(document.getElementById('incorrect').innerText);
+     document.getElementById('incorrect').innerText = ++oldScore;
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+}
+function displaySubtractQuestion(operand1,operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
+
+
+}
+function displayMultiplynQuestion(operand1, operand2) {
+
+     document.getElementById('operand1').textContent = operand1;
+     document.getElementById('operand2').textContent = operand2;
+     document.getElementById('operator').textContent = 'x';
+
+
+
+}
+
+function displayDivisionQuestion(operand1,operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '/';
+}
